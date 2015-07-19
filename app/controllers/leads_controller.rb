434c99244723ab07c629ff6621d12d7572c5c548
Alloffices <alloffices.nyc@gmail.com>
2015-07-19 04:44:15 -0400

    commented out cookies to lead

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -3,6 +3,7 @@ class LeadsController < ApplicationController
 	def create
 		@lead = Lead.new lead_params
 		if @lead.save
+			# cookies[:saved_lead] = true
 			redirect_to root_path, notice: "Saved Successfully!"
 		else
 			redirect_to root_path, alert: "Failed to save"
